60613f530f444b4f65e03ae0855e6b96ffe3ffa4
Indra Lukmana <indralukmana@live.com>
2019-05-09 17:57:35 +0700

    Edit plugin descriptions header

--- plugin.php
+++ plugin.php
@@ -1,15 +1,15 @@
 <?php
 /**
- * Plugin Name: loremtext-block — CGB Gutenberg Block Plugin
- * Plugin URI: https://github.com/ahmadawais/create-guten-block/
- * Description: loremtext-block — is a Gutenberg plugin created via create-guten-block.
- * Author: mrahmadawais, maedahbatool
- * Author URI: https://AhmadAwais.com/
+ * Plugin Name: Loremtext Block
+ * Plugin URI: https://github.com/indralukmana/loremtext-block
+ * Description: A plugin adding a Gutenberg block generating 'lorem ipsum' texts
+ * Author: indralukmana
+ * Author URI: https://www.indralukmana.com
  * Version: 1.0.0
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  *
- * @package CGB
+ * @package loremtext-block
  */
 
 // Exit if accessed directly.
